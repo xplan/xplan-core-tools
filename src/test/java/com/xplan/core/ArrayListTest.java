4c0e7138cd486157d7c4704122b083f65c40a734
Chivalrous <wyhailonger@qq.com>
2011-12-29 11:34:35 +0800

    arraylist test

---
+++ src/test/java/com/xplan/core/ArrayListTest.java
@@ -0,0 +1,33 @@
+package com.xplan.core;  
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+  /**
+   * 
+   *    
+   * ArrayListTest   
+   * test ArrayList   
+   * Chivalrous   
+   * Chivalrous   
+   * 2011-12-29 上午11:24:47   
+   *    
+   * @version 1.0.0   
+   *
+   */
+public class ArrayListTest {
+	
+	 @Test
+	public void testList(){
+		List<Number> l = new ArrayList<Number>();	
+		l.add(4);
+		l.add(6);
+		l.add(7);
+		
+		Assert.assertEquals(4, l.get(0));
+		Assert.assertEquals(6, l.get(1));
+		Assert.assertEquals(7, l.get(2));
+	 }
+}
