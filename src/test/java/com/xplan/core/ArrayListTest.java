01d842167bba8ebbbd1123604a60bd74777a5188
Chivalrous <wyhailonger@qq.com>
2012-01-06 16:56:22 +0800

    test testing

--- src/test/java/com/xplan/core/ArrayListTest.java
+++ src/test/java/com/xplan/core/ArrayListTest.java
@@ -26,6 +26,7 @@ public class ArrayListTest {
 		l.add(6);
 		l.add(7);
 		//test
+		//test is teing 
 		Assert.assertEquals(4, l.get(0));
 		Assert.assertEquals(6, l.get(1));
 		Assert.assertEquals(7, l.get(2));
